1320b0d16c8b8ffb6d0ce1a382cef27df86e5798
Pri Nitta <egashira.sayuri@gmail.com>
2021-03-27 11:52:24 -0300

    converting and printing variables

---
+++ variaveis.py
@@ -0,0 +1,16 @@
+nome = input("Digite seu nome: ")
+print("Bem vindo {}".format(nome))
+
+x = 10.5
+z = 5
+y = "Space"
+
+# exibindo os tipos das variáveis
+print(type(x))
+print(type(z))
+print(type(y))
+
+# convertendo variáveis
+print("Variável float {}".format(x))
+w = int(x)
+print("Mesma variável convertida em int {}".format(w))
