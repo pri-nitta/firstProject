64413e90765b4da69c2eec0558207451cf5bddee
Pri Nitta <egashira.sayuri@gmail.com>
2021-03-28 12:15:35 -0300

    conditions

---
+++ condicao.py
@@ -0,0 +1,7 @@
+funcionario = input("Digite o nome do funcionário: ")
+salario = float(input("Digite o salário de {}: ".format(funcionario)))
+
+if salario < 0:
+    salario = salario * -1
+    print("O salário digitado foi negativo, portanto o corrigimos")
+    print("O salário de {} é {}".format(funcionario, salario))
