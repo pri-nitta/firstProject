b2aa39bb497f392ca218afc76063be214ea4f4d4
Pri Nitta <egashira.sayuri@gmail.com>
2021-04-04 16:13:08 -0300

    While example in class.

---
+++ CAP3/while.py
@@ -0,0 +1,8 @@
+resposta = ""
+
+while resposta != "42":
+    resposta = input("Qual o significado da vida, do universo e tudo mais? ")
+
+print("Parabéns, você é um verdadeiro geek!")
+
+#não entendi a referência, mas blz.
