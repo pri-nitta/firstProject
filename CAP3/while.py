c391fb80566869bef7596fe3e01f0ddce5e299c4
Pri Nitta <egashira.sayuri@gmail.com>
2021-04-04 16:18:12 -0300

    While 2nd example in class.

--- CAP3/while.py
+++ CAP3/while.py
@@ -2,7 +2,13 @@ resposta = ""
 
 while resposta != "42":
     resposta = input("Qual o significado da vida, do universo e tudo mais? ")
-
 print("Parabéns, você é um verdadeiro geek!")
 
 #não entendi a referência, mas blz.
+
+print("===========")
+
+par = 1
+while par%2==1:
+    par = int(input("Digite um número par! "))
+print("Finalmente hein?")
