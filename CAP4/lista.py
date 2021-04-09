3db502a4fc7c6742d0e6291c085c78fd99771c44
pri-nitta <egashira.sayuri@gmail.com>
2021-04-09 17:18:40 -0300

    remove itens from list

--- CAP4/lista.py
+++ CAP4/lista.py
@@ -1,5 +1,7 @@
 lista = ["guitarra", "Violão", "violino"]
 print(lista)
+
+#adicionar itens
 lista.append("bateria")
 print(lista)
 lista.append(input("Digite o novo instrumento: "))
@@ -7,3 +9,13 @@ print(lista)
 
 #colocar o valor em um lugar específico
 lista.insert(0, "baixo")
+print(lista)
+
+#remover o último elemento da lista
+lista.pop()
+print(lista)
+
+#remove um item específico
+#caso o item não seja encontrado, ele gera um erro
+lista.remove("bateria")
+print(lista)
