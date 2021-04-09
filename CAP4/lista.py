bcc3e01666fb3f2bbd2605046e648c17b3350aa3
pri-nitta <egashira.sayuri@gmail.com>
2021-04-09 16:30:54 -0300

    list

---
+++ CAP4/lista.py
@@ -0,0 +1,9 @@
+lista = ["guitarra", "Violão", "violino"]
+print(lista)
+lista.append("bateria")
+print(lista)
+lista.append(input("Digite o novo instrumento: "))
+print(lista)
+
+#colocar o valor em um lugar específico
+lista.insert(0, "baixo")
