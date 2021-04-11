91ef75a0517d10dba8058c657bc98be90341d16e
Pri Nitta <egashira.sayuri@gmail.com>
2021-04-11 18:43:47 -0300

    function update

--- CAP4/funcao.py
+++ CAP4/funcao.py
@@ -7,8 +7,20 @@ somar()
 
 #Usando parâmetros
 def somar2(c, d):
-    print(c+d)
+    total = c + d
+    print(total)
 
 v1 = float(input("Digite o 1º num: "))
 v2 = float(input("Digite o 2º num: "))
 somar2(v1, v2)
+
+#passando diretamente parâmetros na função
+somar2(54,96)
+
+#usando return
+def soma3(x,y):
+    total2 = x + y
+    return (total2)
+
+print("==============================")
+print(soma3(159,753))
