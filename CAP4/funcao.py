5be57386d3ca9191c119b90baba2b8278c858a19
Pri Nitta <egashira.sayuri@gmail.com>
2021-04-11 16:54:03 -0300

    functions

---
+++ CAP4/funcao.py
@@ -0,0 +1,14 @@
+def somar():
+    a = float(input("Digite o primeiro número: "))
+    b = float(input("Digite o segundo número: "))
+    print(a + b)
+
+somar()
+
+#Usando parâmetros
+def somar2(c, d):
+    print(c+d)
+
+v1 = float(input("Digite o 1º num: "))
+v2 = float(input("Digite o 2º num: "))
+somar2(v1, v2)
