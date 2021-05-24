a13614edb65cc444769c3e53b3ef4dbd99bb8b82
Priscila Nitta <priscila.nitta@venturus.org.br>
2021-05-24 11:53:00 -0300

    tuplas and dictionary

---
+++ CAP4/functions.py
@@ -0,0 +1,7 @@
+def somar():
+    a = float(input("Digite um valor: "))
+    b = float(input("Digite outro valor: "))
+    soma = a + b
+    print(soma)
+
+somar()
---
+++ CAP8/tuplas.py
@@ -0,0 +1,19 @@
+usuarios = {}
+emails = ["xpto@email.com", "xkcd@email.com", "abcd@email.com"]
+
+# lista de numeros e emails
+tupla = list(enumerate(emails))
+
+# acrescenta os dados de nome e nível de acesso aos e-mails
+for chave in range(0, len(tupla)):
+    print("Email: ", tupla[chave][1])
+    usuarios[tupla[chave]] = [input("Digite o nome: "), input("Digite o nível de acesso: ")]
+    print("--------------------------------")
+
+# print das informações
+for chave, dado in usuarios.items():
+    print("Usuário:", chave[0])
+    print("Email:", chave[1])
+    print("Nome:", dado[0])
+    print("Nivel:", dado[1])
+    print("")
